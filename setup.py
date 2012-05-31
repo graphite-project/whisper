bd7c74fb954b6f2e85a735839500e3a1811aa300
Michael Leinartas <mleinartas@gmail.com>
2012-05-31 15:32:59 -0500

    Rev to 0.9.10 final

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
   name='whisper',
-  version='0.9.10_pre5',
+  version='0.9.10',
   url='https://launchpad.net/graphite',
   author='Chris Davis',
   author_email='chrismd@gmail.com',
