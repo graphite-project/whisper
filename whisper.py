graphite-project/whisper

commit a8e30fa445ce93ee33cb54787ba898ddd94d2d24

Fix whisper behavior when duplicate timestamps are submitted
https://bugs.launchpad.net/graphite/+bug/973420

whisper.py
@@ -582,6 +582,7 @@ def __archive_update_many(fh,header,archive,points):
   previousInterval = None
   currentString = ""
   for (interval,value) in alignedPoints:
+    if interval == previousInterval: continue
     if (not previousInterval) or (interval == previousInterval + step):
       currentString += struct.pack(pointFormat,interval,value)
       previousInterval = interval

commit ec98f929a154d440e05ed56940b6696ba03f0058

Correct filtering of duplicate values
The change in a8e30fa445 caused the *first* of duplicate values to be
recorded when our documented behavior is the opposite

whisper.py
@@ -621,12 +621,12 @@ def __archive_update_many(fh,header,archive,points):
   step = archive['secondsPerPoint']
   alignedPoints = [ (timestamp - (timestamp % step), value)
                     for (timestamp,value) in points ]
+  alignedPoints = dict(alignedPoints).items() # Take the last val of duplicates
   #Create a packed string for each contiguous sequence of points
   packedStrings = []
   previousInterval = None
   currentString = ""
   for (interval,value) in alignedPoints:
-    if interval == previousInterval: continue
     if (not previousInterval) or (interval == previousInterval + step):
       currentString += struct.pack(pointFormat,interval,value)
       previousInterval = interval