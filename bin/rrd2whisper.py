06afbcd7423dc84454b3c01df913819bd889b7db
Michael Leinartas <mleinartas@gmail.com>
2013-02-03 21:47:37 -0600

    Improve rrd2whisper's handling of rrd files
    Take into account the actual retentions in rrds and make whisper
    archives similarly. Also allow selection of non-AVERAGE rrd archive
    types

--- bin/rrd2whisper.py
+++ bin/rrd2whisper.py
@@ -19,13 +19,30 @@ except ImportError:
 # Ignore SIGPIPE
 signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
+aggregationMethods = whisper.aggregationMethods
+
+# RRD doesn't have a 'sum' or 'total' type
+aggregationMethods.remove('sum')
+
 option_parser = optparse.OptionParser(usage='''%prog rrd_path''')
-option_parser.add_option('--xFilesFactor', default=0.5, type='float')
+option_parser.add_option(
+    '--xFilesFactor',
+    help="The xFilesFactor to use in the output file. " +
+    "Defaults to the input RRD's xFilesFactor",
+    default=None,
+    type='float')
+option_parser.add_option(
+    '--aggregationMethod',
+    help="The consolidation function to fetch from on input and " +
+    "aggregationMethod to set on output. One of: %s" %
+    ', '.join(aggregationMethods),
+    default='average',
+    type='string')
 
 (options, args) = option_parser.parse_args()
 
 if len(args) < 1:
-  option_parser.print_usage()
+  option_parser.print_help()
   sys.exit(1)
 
 rrd_path = args[0]
@@ -35,50 +52,86 @@ try:
 except rrdtool.error, exc:
   raise SystemExit('[ERROR] %s' % str(exc))
 
-seconds_per_point = rrd_info['step']
+seconds_per_pdp = rrd_info['step']
 
-# First get the max retention - we grab the max of all datasources
+# Reconcile old vs new python-rrdtool APIs (yuck)
+# leave consistent 'rras' and 'datasources' lists
 if 'rra' in rrd_info:
   rras = rrd_info['rra']
 else:
-  rra_count = max([ int(key[4]) for key in rrd_info if key.startswith('rra[') ]) + 1
-  rras = [{}] * rra_count
+  rra_indices = []
+  for key in rrd_info:
+    if key.startswith('rra['):
+      index = int(key.split('[')[1].split(']')[0])
+      rra_indices.append(index)
+
+  rra_count = max(rra_indices) + 1
+  rras = []
   for i in range(rra_count):
-    rras[i]['pdp_per_row'] = rrd_info['rra[%d].pdp_per_row' % i]
-    rras[i]['rows'] = rrd_info['rra[%d].rows' % i]
-
-retention_points = 0
-for rra in rras:
-  points = rra['pdp_per_row'] * rra['rows']
-  if points > retention_points:
-    retention_points = points
-
-retention = seconds_per_point * points
+    rra_info = {}
+    rra_info['pdp_per_row'] = rrd_info['rra[%d].pdp_per_row' % i]
+    rra_info['rows'] = rrd_info['rra[%d].rows' % i]
+    rra_info['cf'] = rrd_info['rra[%d].cf' % i]
+    rra_info['xff'] = rrd_info['rra[%d].xff' % i]
+    rras.append(rra_info)
 
 datasources = []
 if 'ds' in rrd_info:
   datasource_names = rrd_info['ds'].keys()
 else:
-  ds_keys = [ key for key in rrd_info if key.startswith('ds[') ]
-  datasources = list(set( key[3:].split(']')[0] for key in ds_keys ))
+  ds_keys = [key for key in rrd_info if key.startswith('ds[')]
+  datasources = list(set(key[3:].split(']')[0] for key in ds_keys))
+
+# Grab the archive configuration
+relevant_rras = []
+for rra in rras:
+  if rra['cf'] == options.aggregationMethod.upper():
+    relevant_rras.append(rra)
+
+if not relevant_rras:
+  err = "[ERROR] Unable to find any RRAs with consolidation function: %s" % \
+        options.aggregationMethod.upper()
+  raise SystemExit(err)
+
+archives = []
+xFilesFactor = options.xFilesFactor
+for rra in relevant_rras:
+  precision = rra['pdp_per_row'] * seconds_per_pdp
+  points = rra['rows']
+  if not xFilesFactor:
+    xFilesFactor = rra['xff']
+  archives.append((precision, points))
 
 for datasource in datasources:
   now = int(time.time())
-  path = rrd_path.replace('.rrd','_%s.wsp' % datasource)
-  whisper.create(path, [(seconds_per_point,retention_points)], xFilesFactor=options.xFilesFactor)
+  path = rrd_path.replace('.rrd', '_%s.wsp' % datasource)
+  try:
+    whisper.create(path, archives, xFilesFactor=xFilesFactor)
+  except whisper.InvalidConfiguration, e:
+    raise SystemExit('[ERROR] %s' % str(e))
   size = os.stat(path).st_size
-  print 'Created: %s (%d bytes)' % (path,size)
+  archiveConfig = ','.join(["%d:%d" % ar for ar in archives])
+  print "Created: %s (%d bytes) with archives: %s" % (path, size, archiveConfig)
 
-  print 'Migrating data'
-  startTime = str(now - retention)
-  endTime = str(now)
-  (time_info,columns,rows) = rrdtool.fetch(rrd_path, 'AVERAGE', '-s', startTime, '-e', endTime)
-  column_index = list(columns).index(datasource)
-  rows.pop() #remove the last datapoint because RRD sometimes gives funky values
+  print "Migrating data"
+  archiveNumber = len(archives) - 1
+  for precision, points in reversed(archives):
+    retention = precision * points
+    endTime = now - now % precision
+    startTime = endTime - retention
+    (time_info, columns, rows) = rrdtool.fetch(
+      rrd_path,
+      options.aggregationMethod.upper(),
+      '-r', str(precision),
+      '-s', str(startTime),
+      '-e', str(endTime))
+    column_index = list(columns).index(datasource)
+    rows.pop()  # remove the last datapoint because RRD sometimes gives funky values
 
-  values = [row[column_index] for row in rows]
-  timestamps = list(range(*time_info))
-  datapoints = zip(timestamps,values)
-  datapoints = filter(lambda p: p[1] is not None, datapoints)
-  print ' migrating %d datapoints...' % len(datapoints)
-  whisper.update_many(path, datapoints)
+    values = [row[column_index] for row in rows]
+    timestamps = list(range(*time_info))
+    datapoints = zip(timestamps, values)
+    datapoints = filter(lambda p: p[1] is not None, datapoints)
+    print ' migrating %d datapoints from archive %d' % (len(datapoints), archiveNumber)
+    archiveNumber -= 1
+    whisper.update_many(path, datapoints)
