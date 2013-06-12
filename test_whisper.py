74011126ed1719979922e74548344a49503e8eb4
Jeff Schroeder <jeffschroeder@computer.org>
2013-06-12 15:11:44 -0400

    Some code style (primarily) updates to the awesome whisper tests

--- test_whisper.py
+++ test_whisper.py
@@ -1,120 +1,123 @@
-import unittest
-import whisper
 import os
 import time
 import random
+import unittest
 
-def fileExists(filename):
-    try:
-        with open(filename) as f:
-            return True
-    except IOError as e:
-        return False
+import whisper
 
 class TestWhisper(unittest.TestCase):
     """
     Testing functions for whisper.
     """
+    db = "db.wsp"
 
     @classmethod
     def setUpClass(cls):
-        # remove old database
+        cls._removedb()
+
+    @classmethod
+    def _removedb(cls):
         try:
-            os.remove("db.wsp")
-            print('removed old db.wsp')
-        except Exception:
+            if os.path.exists(cls.db):
+                os.unlink(cls.db)
+        except (IOError, OSError):
             pass
 
     def test_validateArchiveList(self):
-        """ TESTCASE for blank archive config"""
+        """blank archive config"""
         with self.assertRaises(whisper.InvalidConfiguration):
             whisper.validateArchiveList([])
 
-        """ TESTCASE for duplicate"""
-        whisper.validateArchiveList([(1,60),(60,60)])
+    def test_duplicate(self):
+        """Checking duplicates"""
+        whisper.validateArchiveList([(1, 60), (60, 60)])
         with self.assertRaises(whisper.InvalidConfiguration):
-            whisper.validateArchiveList([(1,60),(60,60),(1,60)])
+            whisper.validateArchiveList([(1, 60), (60, 60), (1, 60)])
 
-        """ TESTCASE for even precision division"""
-        whisper.validateArchiveList([(60,60),(6,60)])
+    def test_even_precision_division(self):
+        """even precision division"""
+        whisper.validateArchiveList([(60, 60), (6, 60)])
         with self.assertRaises(whisper.InvalidConfiguration):
-            whisper.validateArchiveList([(60,60),(7,60)])
+            whisper.validateArchiveList([(60, 60), (7, 60)])
 
-        """ TESTCASE for timespan coverage"""
-        whisper.validateArchiveList([(1,60),(60,60)])
+    def test_timespan_coverage(self):
+        """timespan coverage"""
+        whisper.validateArchiveList([(1, 60), (60, 60)])
         with self.assertRaises(whisper.InvalidConfiguration):
-            whisper.validateArchiveList([(1,60),(10,1)])
+            whisper.validateArchiveList([(1, 60), (10, 1)])
 
-        """TESTCASE for number of points"""
-        whisper.validateArchiveList([(1,60),(60,60)])
+    def test_number_of_points(self):
+        """number of points"""
+        whisper.validateArchiveList([(1, 60), (60, 60)])
         with self.assertRaises(whisper.InvalidConfiguration):
-            whisper.validateArchiveList([(1,30),(60,60)])
+            whisper.validateArchiveList([(1, 30), (60, 60)])
 
     def test_aggregate(self):
-        """TESTCASE for the aggregate functions"""
-        self.assertEqual(whisper.aggregate('min',[1,2,3,4]),1)          # minimum of 1,2,3,4
-        self.assertEqual(whisper.aggregate('max',[1,2,3,4]),4)          # maximum of 1,2,3,4
-        self.assertEqual(whisper.aggregate('last',[3,2,5,4]),4)         # last element in the known values
-        self.assertEqual(whisper.aggregate('sum',[10,2,3,4]),19)        # sum all the values
-        self.assertEqual(whisper.aggregate('average',[1,2,3,4]),2.5)    # average of the elements
+        """aggregate functions"""
+        # min of 1-4
+        self.assertEqual(whisper.aggregate('min', [1, 2, 3, 4]), 1)
+        # max of 1-4
+        self.assertEqual(whisper.aggregate('max', [1, 2, 3, 4]), 4)
+        # last element in the known values
+        self.assertEqual(whisper.aggregate('last', [3, 2, 5, 4]), 4)
+        # sum ALL THE VALUES!
+        self.assertEqual(whisper.aggregate('sum', [10, 2, 3, 4]), 19)
+        # average of the list elements
+        self.assertEqual(whisper.aggregate('average', [1, 2, 3, 4]), 2.5)
         with self.assertRaises(whisper.InvalidAggregationMethod):
-            whisper.aggregate('derp',[12,2,3123,1])
-
+            whisper.aggregate('derp', [12, 2, 3123, 1])
 
     def test_create(self):
-        """TESTCASE for the creating a db, uses info() to validate """
-        retention = [(1,60),(60,60)]
+        """Create a db and use info() to validate"""
+        retention = [(1, 60), (60, 60)]
 
         # check if invalid configuration fails successfully
         with self.assertRaises(whisper.InvalidConfiguration):
-            whisper.create("db.wsp",[])
+            whisper.create(self.db, [])
 
         # create a new db with a valid configuration
-        whisper.create("db.wsp",retention)
+        whisper.create(self.db, retention)
 
         # attempt to create another db in the same file, this should fail
         with self.assertRaises(whisper.InvalidConfiguration):
-            whisper.create("db.wsp",0)
+            whisper.create(self.db, 0)
 
-        info = whisper.info("db.wsp")
+        info = whisper.info(self.db)
 
         # check header information
-        self.assertEqual(info['maxRetention'], max([a[0]*a[1] for a in retention]))
+        self.assertEqual(info['maxRetention'], max([a[0] * a[1] for a in retention]))
         self.assertEqual(info['aggregationMethod'], 'average')
         self.assertEqual(info['xFilesFactor'], 0.5)
 
         # check archive information
-        self.assertEqual(len(info['archives']),len(retention))
-        self.assertEqual(info['archives'][0]['points'],retention[0][1])
-        self.assertEqual(info['archives'][0]['secondsPerPoint'],retention[0][0])
-        self.assertEqual(info['archives'][0]['retention'],retention[0][0]*retention[0][1])
-        self.assertEqual(info['archives'][1]['retention'],retention[1][0]*retention[1][1])
+        self.assertEqual(len(info['archives']), len(retention))
+        self.assertEqual(info['archives'][0]['points'], retention[0][1])
+        self.assertEqual(info['archives'][0]['secondsPerPoint'], retention[0][0])
+        self.assertEqual(info['archives'][0]['retention'], retention[0][0] * retention[0][1])
+        self.assertEqual(info['archives'][1]['retention'], retention[1][0] * retention[1][1])
 
         # remove database
-
-        os.remove("db.wsp")
-
+        self._removedb()
 
     def test_fetch(self):
-        """ TESTCASE for fetching info from database """
+        """ fetch info from database """
 
         # check a db that doesnt exist
         with self.assertRaises(Exception):
-            whisper.fetch("this_db_does_not_exist",0)
+            whisper.fetch("this_db_does_not_exist", 0)
 
         # SECOND MINUTE HOUR DAY
-        retention = [(1,60),(60,60),(3600,24),(86400,365)]
-        whisper.create("db.wsp",retention)
+        retention = [(1, 60), (60, 60), (3600, 24), (86400, 365)]
+        whisper.create(self.db,retention)
 
         # check a db with an invalid time range
         with self.assertRaises(whisper.InvalidTimeInterval):
-            whisper.fetch("db.wsp", time.time(), time.time()-6000)
+            whisper.fetch(self.db, time.time(), time.time()-6000)
 
-        fetch = whisper.fetch("db.wsp",0)
-        # print(fetch)
+        fetch = whisper.fetch(self.db, 0)
 
         # check time range
-        self.assertEqual(fetch[0][1]-fetch[0][0], retention[-1][0]*retention[-1][1])
+        self.assertEqual(fetch[0][1] - fetch[0][0], retention[-1][0] * retention[-1][1])
 
         # check number of points
         self.assertEqual(len(fetch[1]), retention[-1][1])
@@ -122,30 +125,30 @@ class TestWhisper(unittest.TestCase):
         # check step size
         self.assertEqual(fetch[0][2], retention[-1][0])
 
-        os.remove("db.wsp")
+        self._removedb()
 
     def test_update_single_archive(self):
-        """ TEST Update functionality with a single leveled archive"""
+        """Update with a single leveled archive"""
 
-        retention_schema = [(1,20)]
+        retention_schema = [(1, 20)]
         num_data_points = 20
 
-        whisper.create("db.wsp", retention_schema)
+        whisper.create(self.db, retention_schema)
 
         # create sample data
         tn = time.time()-num_data_points
         data = []
         for i in range(num_data_points):
-            data.append((tn+1+i, random.random()*10))
+            data.append((tn + 1 + i, random.random() * 10))
 
         # test single update
-        whisper.update("db.wsp", data[0][1], data[0][0])
+        whisper.update(self.db, data[0][1], data[0][0])
 
         # test multi update
-        whisper.update_many("db.wsp", data[1:])
+        whisper.update_many(self.db, data[1:])
 
         # fetch the data
-        fetch = whisper.fetch("db.wsp",0)   # all data
+        fetch = whisper.fetch(self.db, 0)   # all data
         fetch_data = fetch[1]
 
         for i, (timestamp, value) in list(enumerate(data)):
@@ -155,26 +158,16 @@ class TestWhisper(unittest.TestCase):
         # check TimestampNotCovered
         with self.assertRaises(whisper.TimestampNotCovered):
             # in the future
-            whisper.update("db.wsp", 1.337, time.time()+1)
+            whisper.update(self.db, 1.337, time.time() + 1)
         with self.assertRaises(whisper.TimestampNotCovered):
             # before the past
-            whisper.update("db.wsp", 1.337, time.time()-retention_schema[0][1]-1)
+            whisper.update(self.db, 1.337, time.time() - retention_schema[0][1] - 1)
 
-        os.remove("db.wsp")
-
-
-
-
+        self._removedb()
 
     @classmethod
     def tearDownClass(cls):
-        # make sure file is removed
-        try:
-            os.remove("db.wsp")
-            print('cleaned up db.wsp')
-        except Exception:
-            pass
-
+        cls._removedb()
 
 if __name__ == '__main__':
     unittest.main()
