667f877db9aa5a3e45d9cc8f45146f269954937c
Joerg Fiedler <joerg.fiedler@immobilienscout24.de>
2014-01-17 16:32:11 +0100

    Create and print statistics for processed metrics files (whisper-auto-resize.py)

--- bin/whisper-auto-resize.py
+++ bin/whisper-auto-resize.py
@@ -37,6 +37,9 @@ option_parser.add_option(
 option_parser.add_option(
     '-x', '--extra_args', default='',
     type='string', help="pass any additional arguments to the whisper-resize.py script")
+option_parser.add_option(
+    '-s', '--statistics', default=False, action='store_true',
+    help='Print some statistics')
 
 (options, args) = option_parser.parse_args()
 
@@ -48,7 +51,7 @@ if len(args) < 2:
     sys.exit(1)
 
 storagePath = args[0]
-configPath  = args[1]
+configPath = args[1]
 
 #check to see if we are processing a subfolder
 # we need to have a seperate config option for this since
@@ -84,7 +87,31 @@ from carbon.storage import loadStorageSchemas, loadAggregationSchemas
 schemas = loadStorageSchemas()
 agg_schemas = loadAggregationSchemas()
 
+
 # check to see if a metric needs to be resized based on the current config
+def rebuild_metric(aggregationMethod, fullPath, messages, schema_config_args, xFilesFactor):
+    do_it = options.doit
+    cmd = 'whisper-resize.py %s %s %s --xFilesFactor=%s --aggregationMethod=%s %s' % \
+          (['', '-q'][options.quiet], fullPath, options.extra_args, xFilesFactor, aggregationMethod,
+           schema_config_args)
+
+    if options.confirm:
+        print messages
+        print cmd
+        do_it = confirm("Would you like to run this command? [y/n]: ")
+        if not do_it:
+            print "Skipping command \n"
+
+    if do_it:
+        if call(cmd, shell=True) != 0:
+            Log.error('Error running: %s' % (cmd))
+            return StatisticsCollector.REBUILD_ERROR
+        else:
+            return StatisticsCollector.REBUILD_DONE
+    else:
+        return StatisticsCollector.REBUILD_NOT_REQUIRED
+
+
 def processMetric(fullPath, schemas, agg_schemas):
     """
         method to process a given metric, and resize it if necessary
@@ -96,7 +123,7 @@ def processMetric(fullPath, schemas, agg_schemas):
 
     """
     schema_config_args = ''
-    schema_file_args   = ''
+    schema_file_args = ''
     rebuild = False
     messages = ''
 
@@ -126,7 +153,7 @@ def processMetric(fullPath, schemas, agg_schemas):
 
     # loop through the current files bucket sizes and convert to string format to compare for resizing
     for fileRetention in info['archives']:
-        current_schema  = '%s:%s ' % (fileRetention['secondsPerPoint'], fileRetention['points'])
+        current_schema = '%s:%s ' % (fileRetention['secondsPerPoint'], fileRetention['points'])
         schema_file_args += current_schema
 
     # check to see if the current and configured schemas are the same or rebuild
@@ -135,7 +162,7 @@ def processMetric(fullPath, schemas, agg_schemas):
         messages += 'updating Retentions from: %s to: %s \n' % (schema_file_args, schema_config_args)
 
     Log.info("xFilesFactor: %s (wsp file) / %s (storage-aggregation.cfg)" %
-                  (str(info['xFilesFactor']), str(xFilesFactor)))
+             (str(info['xFilesFactor']), str(xFilesFactor)))
 
     # set xFilesFactor to the wsp files setting if there is nothing configured in storage-aggregation.conf
     if not xFilesFactor:
@@ -144,33 +171,24 @@ def processMetric(fullPath, schemas, agg_schemas):
 
     # only care about the first two decimals in the comparison since there is floaty stuff going on.
     info_xFilesFactor = "{0:.2f}".format(info['xFilesFactor'])
-    str_xFilesFactor =  "{0:.2f}".format(xFilesFactor)
+    str_xFilesFactor = "{0:.2f}".format(xFilesFactor)
 
     # check to see if the current and configured aggregationMethods are the same
     if (str_xFilesFactor != info_xFilesFactor):
         rebuild = True
-        messages += '%s xFilesFactor differs real: %s should be: %s \n' % (metric, info_xFilesFactor, str_xFilesFactor)
+        messages += '%s xFilesFactor differs real: %s should be: %s \n' % \
+                    (metric, info_xFilesFactor, str_xFilesFactor)
     if (aggregationMethod != info['aggregationMethod']):
         rebuild = True
-        messages += '%s aggregation schema differs real: %s should be: %s \n' % (metric, info['aggregationMethod'], aggregationMethod)
+        messages += '%s aggregation schema differs real: %s should be: %s \n' % \
+                    (metric, info['aggregationMethod'], aggregationMethod)
 
     # if we need to rebuild, lets do it.
-    if (rebuild == True):
-        cmd = 'whisper-resize.py %s %s %s --xFilesFactor=%s --aggregationMethod=%s %s' % \
-              (['', '-q'][options.quiet], fullPath, options.extra_args, xFilesFactor, aggregationMethod, schema_config_args)
-        if (options.confirm == True):
-            print messages
-            print cmd
-            options.doit = confirm("Would you like to run this command? [y/n]: ")
-            if (options.doit == False):
-                print "Skipping command \n"
+    if rebuild:
+        return rebuild_metric(aggregationMethod, fullPath, messages, schema_config_args, xFilesFactor)
+    else:
+        return StatisticsCollector.REBUILD_NOT_REQUIRED
 
-        if (options.doit == True):
-            exitcode = call(cmd, shell=True)
-            # if the command failed lets bail so we can take a look before proceeding
-            if (exitcode > 0):
-                print 'Error running: %s' % (cmd)
-                sys.exit(1)
 
 def getMetricFromPath(filePath):
     """
@@ -191,6 +209,7 @@ def getMetricFromPath(filePath):
     Log.info("WORKING ON: " + metric_name)
     return metric_name
 
+
 def confirm(question, error_response='Valid options : yes or no'):
     """
          ask the user if they would like to perform the action
@@ -207,9 +226,42 @@ def confirm(question, error_response='Valid options : yes or no'):
             return False
         print error_response
 
+
+class StatisticsCollector:
+    REBUILD_NOT_REQUIRED = 0
+    REBUILD_DONE = 1
+    REBUILD_ERROR = 2
+    statistics = {}
+
+    def __init__(self):
+        self.statistics[self.REBUILD_DONE] = 0
+        self.statistics[self.REBUILD_ERROR] = 0
+        self.statistics[self.REBUILD_NOT_REQUIRED] = 0
+        pass
+
+    def update_counters(self, status_code):
+        self.statistics[status_code] += 1
+
+    def has_errors(self):
+        return self.statistics[self.REBUILD_ERROR]
+
+    def as_string(self):
+        total = sum(self.statistics.values())
+        return 'Metrics processed: %s, ok: %s, error: %s, not changed: %s' % \
+               (total,
+                self.statistics[self.REBUILD_DONE],
+                self.statistics[self.REBUILD_ERROR],
+                self.statistics[self.REBUILD_NOT_REQUIRED])
+
+statistics_collector = StatisticsCollector()
 for root, _, files in os.walk(processPath):
     # we only want to deal with non-hidden whisper files
     for f in fnmatch.filter(files, '*.wsp'):
         fullpath = os.path.join(root, f)
-        processMetric(fullpath, schemas, agg_schemas)
+        status = processMetric(fullpath, schemas, agg_schemas)
+        statistics_collector.update_counters(status)
 
+if options.statistics:
+    print statistics_collector.as_string()
+
+sys.exit(0 if not statistics_collector.has_errors() else 1)
