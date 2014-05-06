c4cf8057696c4c32de001b81646e7ad06788a305
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-05-06 21:31:04 +0000

    Now whisper-merge is able to work with directories. Closes #73.

--- bin/whisper-merge.py
+++ bin/whisper-merge.py
@@ -4,6 +4,7 @@ import os
 import sys
 import signal
 import optparse
+import shutil
 
 try:
   import whisper
@@ -25,8 +26,27 @@ if len(args) < 2:
 path_from = args[0]
 path_to = args[1]
 
+
 for filename in (path_from, path_to):
    if not os.path.exists(filename):
        raise SystemExit('[ERROR] File "%s" does not exist!' % filename)
 
-whisper.merge(path_from, path_to)
+
+if os.path.isdir(path_from) and os.path.isdir(path_to):
+  for root, dirs, files in os.walk(path_from):
+    for file in files:
+      fullname = os.path.join(root, file)
+      relpath = os.path.relpath(fullname, path_from)
+      other_file = os.path.join(path_to, relpath)
+      
+      if os.path.exists(other_file):
+        print 'Merging', relpath
+        whisper.merge(fullname, other_file)
+      else:
+        dirname = os.path.dirname(other_file)
+        if not os.path.exists(dirname):
+          os.makedirs(dirname)
+        print 'Copying', relpath, 'to', path_to
+        shutil.copy(fullname, other_file)
+else:
+   whisper.merge(path_from, path_to)
