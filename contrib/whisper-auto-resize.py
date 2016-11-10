graphite-project/whisper

commit 32b1b6b4d9bcd619f954f808eb352d17e6e2408b

contrib, whisper-auto-resize: define default values for xFilesFactor and aggregationMethod
trying to fix the following trace:

Traceback (most recent call last):
  File "/tmp/whisper-auto-resize.py", line 208, in <module>
    processMetric(fullpath, schemas, agg_schemas)
  File "/tmp/whisper-auto-resize.py", line 136, in processMetric
    str_xFilesFactor =  "{0:.2f}".format(xFilesFactor)
ValueError: Unknown format code 'f' for object of type 'str'

when xFilesFactor and aggregationMethod is not defined in configuration for a specfic data subdir
use default values defined in documentation

Signed-off-by: William Dauchy <w.dauchy@criteo.com>

contrib/whisper-auto-resize.py
@@ -109,9 +109,12 @@ def processMetric(fullPath, schemas, agg_schemas):
             archive_config = [archive.getTuple() for archive in schema.archives]
             break
 
+    xFilesFactor = 0.5
+    aggregationMethod = 'average'
+
     # loop through the carbon-aggregation schemas
     for agg_schema in agg_schemas:
-        if agg_schema.matches(metric):
+        if agg_schema.matches(metric) and all(x is not None for x in agg_schema.archives):
             xFilesFactor, aggregationMethod = agg_schema.archives
             break
 

commit b9e1be91e4d15b24b3b49a1d601db35230d2f89f

whisper-auto-resize: fix default values for xFilesFactor and aggregationMethod
the condition was wrong when agg_schema was either defined for xFilesFactor
or aggregationMethod
in this case the default values were always used

Test each value separately to resolve the issue

Fixes: 32b1b6b4d9b (contrib, whisper-auto-resize: define default values
       for xFilesFactor and aggregationMethod)
Signed-off-by: William Dauchy <w.dauchy@criteo.com>

contrib/whisper-auto-resize.py
@@ -119,14 +119,16 @@ def processMetric(fullPath, schemas, agg_schemas):
             archive_config = [archive.getTuple() for archive in schema.archives]
             break
 
-    xFilesFactor = 0.5
-    aggregationMethod = 'average'
-
     # loop through the carbon-aggregation schemas
     for agg_schema in agg_schemas:
-        if agg_schema.matches(metric) and all(x is not None for x in agg_schema.archives):
+        if agg_schema.matches(metric):
             xFilesFactor, aggregationMethod = agg_schema.archives
             break
+
+    if xFilesFactor is None:
+        xFilesFactor = 0.5
+    if aggregationMethod is None:
+        aggregationMethod = 'average'
 
     # loop through the bucket tuples and convert to string format for resizing
     for retention in archive_config: