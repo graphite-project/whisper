6e724d90b129dd91ea217d94d5c875c8dda28163
Corentin Chary <c.chary@criteo.com>
2017-03-21 15:28:30 +0100

    1.0.0-pre1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
   name='whisper',
-  version='0.10.0-rc1',
+  version='1.0.0-pre1',
   url='http://graphiteapp.org/',
   author='Chris Davis',
   author_email='chrismd@gmail.com',
