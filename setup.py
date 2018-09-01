17ff60b7aacfa41f14e973ad6614366de4d1432b
Denis Zhdanov <denis.zhdanov@gmail.com>
2018-09-01 15:21:46 +0200

    Prepare release 1.1.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
   name='whisper',
-  version='1.1.3',
+  version='1.1.4',
   url='http://graphiteapp.org/',
   author='Chris Davis',
   author_email='chrismd@gmail.com',
