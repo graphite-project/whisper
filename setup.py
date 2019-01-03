1695d4a138673e37653194ce8da2ddfa898eab5c
YevhenLukomskyi <Yevhen.Lukomskyi@gmail.com>
2019-01-03 22:48:35 +0200

    set package long description

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
+import os
 from glob import glob
 from distutils.core import setup
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
 
 
 setup(
@@ -12,6 +18,8 @@ setup(
   author_email='chrismd@gmail.com',
   license='Apache Software License 2.0',
   description='Fixed size round-robin style database',
+  long_description=read('README.md'),
+  long_description_content_type='text/markdown',
   py_modules=['whisper'],
   scripts=glob('bin/*') + glob('contrib/*'),
   install_requires=['six'],
