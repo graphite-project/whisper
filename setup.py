983e358e034e3600088ac7f38ef8af2d11fa88b4
Denis Zhdanov <denis.zhdanov@gmail.com>
2019-10-24 12:50:48 +0200

    preparing 1.1.6 release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
   name='whisper',
-  version='1.1.5',
+  version='1.1.6',
   url='http://graphiteapp.org/',
   author='Chris Davis',
   author_email='chrismd@gmail.com',
